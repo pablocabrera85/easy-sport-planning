6b451a06e007d1f8e86ae38da566474d13bcc29c
Pablo Cabrera <pablocabrera@MacBook-Pro-de-Pablo-Cabrera.local>
2013-06-27 23:49:10 -0300

    Initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasySportPlanning::Application.config.secret_token = 'd47e5d66069f8bd39174cd7faf90557660829e4ec68cc22a0e4405f58a35a0ddf36f1e128a85b0db90741b2a649ae72313f033afa2fb9c9ba336a7fdba26a9ae'
